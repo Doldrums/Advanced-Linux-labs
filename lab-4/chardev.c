2a6317a4b3c2cab5c40c6e4d2acffe251572258f
doldrums <doldrums.317@gmail.com>
2022-12-11 22:54:38 +0300

    lab4

--- lab-4/chardev.c
+++ lab-4/chardev.c
@@ -21,10 +21,6 @@
 #include <linux/kernel.h> /* We are doing kernel work */ 
 #include <linux/module.h> /* Specifically, a module */ 
 #include <linux/poll.h> 
-#include <stdio.h>
-#include <stdlib.h>
-#include <errno.h>
- 
 #include "chardev.h" 
 #define SUCCESS 0 
 #define DEVICE_NAME "char_dev" 
@@ -49,64 +45,8 @@ struct node
  
 int MAX = 5;
  
-void push();
-void pop();
-void stack_size();
- 
-// to insert elements in stack
-void push(int32_t &val)
-{
-    int count;
-    struct node *temp;
-    temp = (struct node*)malloc(sizeof(struct node));
- 
-    count = st_count();
-    if (count <= MAX - 1)
-    {
-        temp->data = val;
-        temp->link = top;
-        top = temp;
-    }
-    else {
-        errno = ENOSPC;
-	    return 0;
-    }
-}
- 
-// to delete elements from stack
-void pop()
-{
-    struct node *temp;
-    if (top == NULL){
-        errno = ENODEV;
-	    return 0;
-    }
-    else
-    {
-        temp = top;
-        printf("Value popped out is %d \n",temp->data);
-        top = top->link;
-        free(temp);
-    }
-}
- 
-// to count the number of elements
-void stack_size()
-{
-    int count = 0;
-    struct node *temp;
- 
-    temp = top;
-    while (temp != NULL)
-    {
-        printf(" %d\n",temp->data);
-        temp = temp->link;
-        count++;
-    }
-    printf("size of stack is %d \n",count);
-}
- 
-int st_count()
+
+int st_count(void)
 {
     int count = 0;
     struct node *temp;
@@ -117,6 +57,60 @@ int st_count()
         count++;
     }
     return count;
+}
+
+// to insert elements in stack
+int push(int32_t val)
+{
+    int result = 0;
+    int count;
+    struct node *temp;
+    count = st_count();
+    if (count <  MAX)
+    {
+        temp = (struct node*)kmalloc(sizeof(struct node), GFP_KERNEL);
+        temp->data = val;
+        temp->link = top;
+        top = temp;
+    }
+    else {
+      result = -1;
+    }
+    return result;
+}
+ 
+// to delete elements from stack
+int pop(void)
+{
+    int result = 0;
+    struct node *temp;
+    if (top == NULL){
+      result = -1;
+    }
+    else
+    {
+        temp = top;
+        printk("Value popped out is %d \n",temp->data);
+        top = top->link;
+        kfree(temp);
+    }
+    return result;
+}
+ 
+// to count the number of elements
+void print_stack(void)
+{
+    int count = 0;
+    struct node *temp;
+ 
+    temp = top;
+    while (temp != NULL)
+    {
+        printk(" %d\n",temp->data);
+        temp = temp->link;
+        count++;
+    }
+    printk("size of stack is %d \n",count);
 }
 
 static int device_open(struct inode *inode, struct file *file) 
@@ -149,7 +143,10 @@ static ssize_t device_read(struct file *file,
     } 
     pr_info("Read %d bytes, %ld left\n", bytes_read, length); 
     *offset += bytes_read; 
-    pop();
+    int result = pop();
+    if (result != 0) {
+        printk(KERN_ALERT "Error: Something went wrong\n");
+    }
     return bytes_read; 
 } 
  
@@ -160,8 +157,10 @@ static ssize_t device_write(struct file *file, const char __user *buffer,
     pr_info("device_write(%p,%p,%ld)", file, buffer, length); 
     for (i = 0; i < length && i < BUF_LEN; i++) 
         get_user(message[i], buffer + i); 
- 
-    push((int32_t) message);
+    int result =  push((int32_t) message);;
+    if (result != 0) {
+        printk(KERN_ALERT "Error: Something went wrong\n");
+    }
     return i; 
 } 
  
